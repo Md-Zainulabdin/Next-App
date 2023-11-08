a9eacd993eac75afb57637aede21436025e0855e
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-11-08 15:37:19 +0500

    create a weather app

--- src/app/components/WeatherApp.jsx
+++ src/app/components/WeatherApp.jsx
@@ -1,9 +1,12 @@
 "use client";
-import React, { useRef } from "react";
+
+import React, { useRef, useState } from "react";
 import { FiSearch } from "react-icons/fi";
 
 const WeatherApp = () => {
   const searchRef = useRef();
+  const [weather, setWeather] = useState([]);
+  const apiKey = "6d055e39ee237af35ca066f35474e9df";
 
   const onSubmitHandler = async (e) => {
     e.preventDefault(); // to prevent the page load when form is submit
@@ -12,19 +15,19 @@ const WeatherApp = () => {
       return;
     }
 
-    console.log(process.env.API_KEY);
-
     try {
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${searchRef.current.value}&appid=${process.env.API_KEY}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?q=${searchRef.current.value}&units=metric&appid=${apiKey}`
       );
       const data = await response.json();
-
+      setWeather(data);
       console.log("data", data);
     } catch (error) {
       console.log(error);
     }
   };
+
+  // console.log(weather);
 
   return (
     <div className="w-full h-screen flex justify-center items-center">
@@ -46,6 +49,12 @@ const WeatherApp = () => {
             </button>
           </form>
         </div>
+
+        <div className="weather-showcase w-full text-center py-6">
+          {/* <img src={weather.image} alt="" /> */}
+          <h1 className="text-3xl font-bold text-center">{weather?.main?.temp}°C</h1> 
+          <span className="text-xl font-semibold text-[#555]">{weather?.name}</span>
+        </div>
       </div>
     </div>
   );
@@ -55,3 +64,5 @@ export default WeatherApp;
 
 // key: "{Your API Key Here}",
 // base: "https://api.openweathermap.org/data/2.5/",
+
+// ? -> nullish operator || safety operator
