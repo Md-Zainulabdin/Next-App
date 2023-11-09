15928e7b0ab1269666c1e93fb444643c1fd251b7
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-11-09 15:45:51 +0500

    update weather app

--- src/app/components/WeatherApp.jsx
+++ src/app/components/WeatherApp.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useRef, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import { FiSearch } from "react-icons/fi";
 
 const WeatherApp = () => {
@@ -21,16 +22,18 @@ const WeatherApp = () => {
       );
       const data = await response.json();
       setWeather(data);
-      console.log("data", data);
+      // console.log("data", data);
+      toast.success("Api call successfully");
     } catch (error) {
-      console.log(error);
+      toast.error("Something Went Wrong");
     }
   };
 
-  // console.log(weather);
+  console.log(weather.length);
 
   return (
     <div className="w-full h-screen flex justify-center items-center">
+      <Toaster />
       <div className="weather-card border-2 w-[400px] p-8">
         <div className="searchBar w-full">
           <form onSubmit={onSubmitHandler} className="flex gap-2 items-center">
@@ -50,11 +53,21 @@ const WeatherApp = () => {
           </form>
         </div>
 
-        <div className="weather-showcase w-full text-center py-6">
-          {/* <img src={weather.image} alt="" /> */}
-          <h1 className="text-3xl font-bold text-center">{weather?.main?.temp}°C</h1> 
-          <span className="text-xl font-semibold text-[#555]">{weather?.name}</span>
-        </div>
+        {weather.length === 0 ? (
+          <div className="text-center py-6">
+            <h1>Nothing to Show</h1>
+          </div>
+        ) : (
+          <div className="weather-showcase w-full text-center py-6">
+            {/* <img src={weather.image} alt="" /> */}
+            <h1 className="text-3xl font-bold text-center">
+              {weather?.main?.temp}°C
+            </h1>
+            <span className="text-xl font-semibold text-[#555]">
+              {weather?.name}
+            </span>
+          </div>
+        )}
       </div>
     </div>
   );
